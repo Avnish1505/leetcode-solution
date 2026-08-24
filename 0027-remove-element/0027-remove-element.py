ca57d77ad3628dbc43e1e5feb1a47c908b5c32b3
Avnish Singh <avnishsingh150606@gmail.com>
2026-08-24 23:47:12 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.52%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0  # Writer pointer for non-val elements
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+                
+        return k
